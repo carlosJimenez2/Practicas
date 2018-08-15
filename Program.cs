ef37bb6f16a6feed1f66ee3a1ea016c48eff37ad
[carlosJimenez2] <[carlos.cj998@gmail.com]>
2018-08-14 21:18:02 -0500

    comentario

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mascota
+{
+    class Program
+    {
+        enum TIPOSDECOMIDA { CROQUETAS, BASURA, AGUA }
+        static void Main(string[] args)
+        {
+            bool alimentado = false;
+
+            Prro Prro1 = new Prro();
+
+            Prro1.Raza = " French Puddle ";
+            Prro1.Peso = 7;
+            Prro1.Sexo = "Macho";
+            Prro1.Tamaño = "Pequeña";
+
+            alimentado = Prro1.Comer((int)TIPOSDECOMIDA.BASURA);
+
+            if (alimentado)
+            {
+                Console.WriteLine("El perro " + Prro1.Raza + " comio " + Convert.ToString(TIPOSDECOMIDA.AGUA));
+            }
+            else
+            {
+                Console.WriteLine("El perro no ha comido");
+
+            }
+            
+
+            Prro Prro2 = new Prro();
+            Console.WriteLine("El perro " + Prro2.Raza + " comio " + Convert.ToString(TIPOSDECOMIDA.AGUA));
+          
+            Prro Prro3 = new Prro("Shitzu", 15, "Hembra", "pequeño");
+            Console.WriteLine("El perro " + Prro3.Raza + " comio " + Convert.ToString(TIPOSDECOMIDA.AGUA));
+            Console.Read();
+
+
+        }
+    }
+}
---
+++ Prro.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mascota
+{
+    class Prro
+    {
+        public string Tamaño;
+        public string Raza;
+        public string Sexo;
+        public int Peso;
+        public string Color;
+        enum TIPOSDECOMIDA{CROQUETAS, BASURA, AGUA}
+
+        public Prro()
+        {
+            Raza = " Pitbull ";
+            Peso = 8;
+            Sexo = "Macho";
+            Tamaño = "Grande";
+        }
+
+        public Prro(string raza, int peso, string sexo, string tamaño)
+
+        {
+            Raza = raza;
+            Peso = peso;
+            Sexo = sexo;
+            Tamaño = tamaño;
+        }
+
+
+
+
+        public void Ladrar()
+        {
+
+        }
+
+        public void Correr()
+        {
+
+        }
+
+        public bool Comer(int alimento)
+        {
+            bool resultado = false;
+            if (alimento == (int)TIPOSDECOMIDA.AGUA)
+            {
+                resultado = false;
+            }
+            else if (alimento == (int)TIPOSDECOMIDA.BASURA)
+            {
+                resultado = true;
+            }
+            else if (alimento == (int)TIPOSDECOMIDA.CROQUETAS)
+            {
+                resultado = true;
+            }
+            return resultado;
+
+        }
+
+        public void Cagar()
+        {
+
+        }
+
+
+    }
+}
